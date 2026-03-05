4486cacb942a0b3efa3ec05a779b6fc51b8c27e8
Marcelo <marcelo@Marcelos-MacBook-Pro.local>
2026-03-04 22:53:07 -0500

    feat: adding clase-1 clase-2 folders

---
+++ clase-2/index.js
@@ -0,0 +1,62 @@
+// #Ejercicio 1
+
+// const number = prompt("Escribe un numero: ");
+
+// if (number % 2 === "0") {
+//   console.log("El numero es par");
+// } else {
+//   console.log("El numero es impar");
+// }
+
+// #Ejercicio 2
+
+// const a = 30;
+// const b = 30;
+
+// if (typeof a != "number" || typeof b != "number") {
+//   console.log("A o B no son valores numericos");
+// } else if (a > b) {
+//   console.log(`a:${a} es mayor que b:${b}`);
+// } else if (a === b) {
+//   console.log(`a:${a} es igual a b:${b}`);
+// } else {
+//   console.log("El valor de a es menor que b");
+// }
+
+// #Ejercicio 3
+// const a = 2;
+// const b = 3;
+
+// const operator = prompt("Ingresa el operador (* , / , + , -): ");
+
+// switch (operator) {
+//   case "+":
+//     console.log(`La suma es: ${a + b}`);
+//     break;
+
+//   case "-":
+//     console.log(`La resta es: ${a - b}`);
+//     break;
+
+//   case "*":
+//     console.log(`La producto es: ${a * b}`);
+//     break;
+
+//   case "/":
+//     console.log(`La division es: ${a / b}`);
+//     break;
+
+//   default:
+//     console.log("Operacion invalida");
+//     break;
+// }
+
+// #Ejercicio 5
+
+// const userAge = prompt("Ingresa tu edad: ");
+
+// if (userAge >= 18) {
+//   console.log("Eres apto para votar");
+// } else {
+//   console.log("No eres apto para votar");
+// }
